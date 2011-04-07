643dbe7ade1bb219f7c67b2d926e42ceadcaaec9
Anton Berezin <tobez@tobez.org>
2011-04-07 20:40:58 +0200

    Formatting fix.

--- soa.c
+++ soa.c
@@ -11,26 +11,26 @@
 
 static void* soa_parse(char *name, long ttl, int type, char *s)
 {
-    struct rr_soa *rr = getmem(sizeof(*rr));
+	struct rr_soa *rr = getmem(sizeof(*rr));
 
-    rr->mname = extract_name(&s, "mname");
-    if (!rr->mname) return NULL;
-    rr->rname = extract_name(&s, "rname");
-    if (!rr->rname) return NULL;
-    rr->serial = extract_integer(&s, "serial");
-    if (rr->serial < 0) return NULL;
-    rr->refresh = extract_timevalue(&s, "refresh");
-    if (rr->refresh < 0) return NULL;
-    rr->retry = extract_timevalue(&s, "retry");
-    if (rr->retry < 0) return NULL;
-    rr->expire = extract_timevalue(&s, "expire");
-    if (rr->expire < 0) return NULL;
-    rr->minimum = extract_timevalue(&s, "minimum");
-    if (rr->minimum < 0) return NULL;
-    if (*s) {
-	return bitch("garbage after valid SOA data");
-    }
-    return store_record(type, name, ttl, rr);
+	rr->mname = extract_name(&s, "mname");
+	if (!rr->mname) return NULL;
+	rr->rname = extract_name(&s, "rname");
+	if (!rr->rname) return NULL;
+	rr->serial = extract_integer(&s, "serial");
+	if (rr->serial < 0) return NULL;
+	rr->refresh = extract_timevalue(&s, "refresh");
+	if (rr->refresh < 0) return NULL;
+	rr->retry = extract_timevalue(&s, "retry");
+	if (rr->retry < 0) return NULL;
+	rr->expire = extract_timevalue(&s, "expire");
+	if (rr->expire < 0) return NULL;
+	rr->minimum = extract_timevalue(&s, "minimum");
+	if (rr->minimum < 0) return NULL;
+	if (*s) {
+		return bitch("garbage after valid SOA data");
+	}
+	return store_record(type, name, ttl, rr);
 }
 
 static char* soa_human(void *rrv)
