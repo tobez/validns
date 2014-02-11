dd75b6c1769d810f880d6f437310fd2f159b3526
Anton Berezin <tobez@tobez.org>
2014-02-11 21:47:45 +0100

    Update copyright years.

--- naptr.c
+++ naptr.c
@@ -1,7 +1,7 @@
 /*
  * Part of DNS zone file validator `validns`.
  *
- * Copyright 2011-2013 Anton Berezin <tobez@tobez.org>
+ * Copyright 2011-2014 Anton Berezin <tobez@tobez.org>
  * Modified BSD license.
  * (See LICENSE file in the distribution.)
  *
